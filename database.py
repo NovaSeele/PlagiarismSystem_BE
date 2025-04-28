51ce8ebe99bd93580a2a3ac57f093085ba444ee5
Nguyễn Hoàng Anh Dũng <iovn1259@gmail.com>
2025-04-28 18:04:57 +0700

    change mongoDB from localhost into online

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 # from motor.motor_asyncio import AsyncIOMotorClient
 # from pymongo.server_api import ServerApi
 
-# uri = "mongodb+srv://NovaSeele:Anhdungbadao%40262003@batoreach.39u4i.mongodb.net/"
+uri = "mongodb+srv://NovaSeele:Anhdungbadao%40262003@batoreach.39u4i.mongodb.net/"
 
 # uri = "mongodb://localhost:27017/"
 
@@ -13,7 +13,6 @@
 from pymongo import MongoClient
 
 # Kết nối MongoDB với pymongo
-uri = "mongodb://localhost:27017/"
 client = MongoClient(uri)
 
 def get_mongodb_client():
