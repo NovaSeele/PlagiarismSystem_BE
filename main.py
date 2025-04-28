e49c600b3f20124f17b5a99f68ee1ae66b2e329d
Nguyễn Hoàng Anh Dũng <iovn1259@gmail.com>
2025-04-29 00:06:51 +0700

    minor port fix

--- main.py
+++ main.py
@@ -30,4 +30,5 @@ def read_root():
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+    port = int(os.environ.get("PORT", 8000))  # lấy port từ biến môi trường
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
