f99a6dd67b68c40a4c996c790ed9bf1d636d4f97
Nguyễn Hoàng Anh Dũng <iovn1259@gmail.com>
2025-04-28 22:19:04 +0700

    change host for render

--- main.py
+++ main.py
@@ -31,4 +31,4 @@ def read_root():
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host=settings.API_HOST, port=settings.API_PORT, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8888, reload=True)
