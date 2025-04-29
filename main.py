1ef9bee65762fe3b20b2124238a00d4414017971
Nguyễn Hoàng Anh Dũng <iovn1259@gmail.com>
2025-04-29 18:40:31 +0700

    ngrok minor fix

--- main.py
+++ main.py
@@ -10,10 +10,9 @@ app = FastAPI()
 # Biến global để lưu trữ URL ngrok
 NGROK_URL = None
 
-# Cấu hình CORS (cho phép tất cả nguồn trong môi trường dev)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Thay bằng URL ngrok cụ thể nếu cần bảo mật
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -36,7 +35,7 @@ def get_ngrok_url():
 
 if __name__ == "__main__":
     # Khởi tạo ngrok
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8888))
     ngrok_tunnel = ngrok.connect(port)  # Tạo tunnel
     public_url = ngrok_tunnel.public_url
 
@@ -48,4 +47,4 @@ if __name__ == "__main__":
     # Chạy ứng dụng
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("main:app", host=settings.API_HOST, port=port, reload=True)
