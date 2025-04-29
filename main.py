e326bf95c94f898644cda2c44b346caf62e0a91b
Nguyễn Hoàng Anh Dũng <iovn1259@gmail.com>
2025-04-29 18:42:13 +0700

    some other fix

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ NGROK_URL = None
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:5173/*", "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -35,16 +35,16 @@ def get_ngrok_url():
 
 if __name__ == "__main__":
     # Khởi tạo ngrok
-    port = int(os.environ.get("PORT", 8888))
-    ngrok_tunnel = ngrok.connect(port)  # Tạo tunnel
-    public_url = ngrok_tunnel.public_url
+    # port = int(os.environ.get("PORT", 8888))
+    # ngrok_tunnel = ngrok.connect(port)  # Tạo tunnel
+    # public_url = ngrok_tunnel.public_url
 
-    # Lưu URL vào biến global
-    NGROK_URL = public_url
+    # # Lưu URL vào biến global
+    # NGROK_URL = public_url
 
-    print(f"Access your app here: {public_url}")
+    # print(f"Access your app here: {public_url}")
 
     # Chạy ứng dụng
     import uvicorn
 
-    uvicorn.run("main:app", host=settings.API_HOST, port=port, reload=True)
+    uvicorn.run("main:app", host=settings.API_HOST, port=settings.API_PORT, reload=True)
